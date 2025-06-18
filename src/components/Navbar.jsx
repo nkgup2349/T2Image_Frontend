fbbaf5fffbe294b043e599c8db8657ba64f0781c
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2025-06-18 21:54:59 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                 </button>
                 <p className='text-white max-sm:hidden pl-4'>Hi , {user.name}</p>
                 <div className='relative group'>
-                    <img src={assets.profile_icon} className='w-10 drop-shadow  border-4 border-black rounded-full' />
+                    <img src={assets.profile_icon} className='w-10 drop-shadow max-sm:ml-1 border-4 border-black rounded-full' />
                     <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                         <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
                             <li className='py-1  px-2 cursor-pointer pr-10'  onClick={logout}>LogOut</li>
